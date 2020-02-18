2bc944c1b86835290c4c23c02cc4e8a0f9b0ad97
Ben <benlaugherty@gmail.com>
2020-02-18 00:50:38 -0800

    add working checkin :)

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/antchfx/htmlquery v1.2.2
 	github.com/antchfx/xpath v1.1.4 // indirect
+	github.com/imroc/req v0.3.0
 	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
@@ -14,7 +15,6 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.6.2 // indirect
 	golang.org/x/sys v0.0.0-20200212091648-12a6c2dcc1e4 // indirect
-	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/ini.v1 v1.52.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- modules/checkin.go
+++ modules/checkin.go
@@ -2,6 +2,7 @@ package modules
 
 import (
 	"fmt"
+	"github.com/imroc/req"
 	"github.com/levigross/grequests"
 	"log"
 	"strings"
@@ -17,75 +18,54 @@ func CheckinModule(cmdCtx *cobra.Command, args[]string) {
 		return
 	}
 
-	// s represents our simple session wrapper that sets up request options and such
-	s := NewSession()
+	// Create a new session
+	session := req.New()
+
 	// loginURL is just the url we are getting/posting to to log the user in
 	loginURL := "https://www.makeschool.com/login"
 
 	// Get the login page and check for errors
-	loginPage, err := s.Session.Get(loginURL, s.DefaultRO)
+	_, err := session.Get(loginURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	param := req.Param{
+		"user[email]": "email",
+		"user[password]": "password",
+	}
+
+	header := req.Header{
+		"Content-Type": "application/x-www-form-urlencoded",
+		"User-Agent": "MSConsole - https://github.comn/BenAndGarys/msconsole-go",
+	}
+
+	resp, err := session.Post(loginURL, param, header)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Make sure we get a 200 status code from our request
-	if loginPage.Ok == false {
-		log.Fatalf("Got non-ok status code from response... Code: %d", loginPage.StatusCode)
+	if resp.Response().StatusCode != 200 {
+		log.Fatalf("Got non-ok status code from response... Code: %d", resp.Response().StatusCode)
 	}
 
-	// Create a map with the info we want to login with
-	formData := map[string]string{
-		"user[email]": "benlaugherty@gmail.com", // Make school dashboard login here
-		"user[password]": "{RfV.4G8{Kv>tU82zYbR", // Make school password here
-	}
+	// Get the banner message from the response body
+	bannerMessage := getBannerMessage(resp.String())
 
-	// Parse string into html node so we can parse
-	htmlData, err := htmlquery.Parse(strings.NewReader(loginPage.String()))
+	if bannerMessage != "Signed in successfully." {
+		log.Fatal(bannerMessage)
+	}
+	fmt.Println(bannerMessage)
+
+	// Try to log the user in
+	resp, err = session.Get(fmt.Sprintf("http://make.sc/attend/%s", args[0]), &grequests.RequestOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Find all hidden inputs
-	nodes := htmlquery.Find(htmlData, "//form//input[@type='hidden']")
-
-	// Get authenticity token
-	for _, node := range nodes {
-		for i, item := range node.Attr {
-			if item.Val == "authenticity_token" {
-				formData[item.Val] = node.Attr[i + 1].Val
-			}
-		}
-	}
-
-	fmt.Println(formData)
-
-	// Post that data to the login page, and get the response
-	resp, err := s.Session.Post(loginURL, &grequests.RequestOptions{Data:formData, RedirectLimit:3})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(resp.StatusCode)
-
-	resp, err = s.Session.Get(fmt.Sprintf("http://make.sc/attend/%s", args[0]), &grequests.RequestOptions{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// Print the new banner message.
 	fmt.Println(getBannerMessage(resp.String()))
-}
-
-// Session represents stuff to do with making requests and keeping a session to do so
-type Session struct {
-	Session *grequests.Session
-	DefaultRO *grequests.RequestOptions
-}
-
-func NewSession() *Session {
-	ro := &grequests.RequestOptions{RedirectLimit:100}
-	session := grequests.NewSession(ro)
-
-	return &Session{Session:session, DefaultRO:ro}
 }
 
 func getBannerMessage(page string) string {
