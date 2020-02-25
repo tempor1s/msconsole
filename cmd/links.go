8fa70fc9cd461b7106a03a4c2da4b8eec66d4b64
Ben <benlaugherty@gmail.com>
2020-02-24 20:20:20 -0800

    update links

--- cmd/links.go
+++ cmd/links.go
@@ -11,10 +11,10 @@ func init() {
 }
 
 var linksCommand = &cobra.Command{
-	Use: "links",
+	Use:   "links",
 	Short: "Get all links for a class.",
-	Long: "Given a class code, this command will give you a list of useful links to a class.",
-	Run: func(cmd *cobra.Command, args[]string) {
+	Long:  "Given a class code, this command will give you a list of useful links to a class.",
+	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Implement
 		fmt.Println("MSConsole's links command. Coming soon! :)")
 	},
--- cmd/version.go
+++ cmd/version.go
@@ -11,10 +11,10 @@ func init() {
 }
 
 var versionCommand = &cobra.Command{
-	Use: "version",
+	Use:   "version",
 	Short: "Print the version for MSConsole",
-	Long: "All software has versions. This is MSConsole's",
-	Run: func(cmd *cobra.Command, args[]string) {
+	Long:  "All software has versions. This is MSConsole's",
+	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Pull from somewhere
 		fmt.Println("MSConsole's version is currently alpha")
 	},
