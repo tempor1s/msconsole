99a54d1d6ee84759fed96cf72cf35da6e229024c
Ben <benlaugherty@gmail.com>
2020-02-27 01:40:47 -0800

    small update to server / docker stuff

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ COPY ./ /app
 RUN go mod download
 RUN go get github.com/githubnemo/CompileDaemon
 
+EXPOSE 5000
+
 ENTRYPOINT CompileDaemon --build="go build ./server/main.go" --command=./main -log-prefix=false
--- server/main.go
+++ server/main.go
@@ -8,10 +8,28 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/log", logCheckin)
+
+	// Just basic home page welcome message
+	e.GET("/", homeRoute)
+	// Add one to amount of times checked in.
+	e.POST("/log", logCheckinRoute)
+	// Get the amount of times checked in
+	e.GET("/log", getCheckinCount)
+
 	e.Logger.Fatal(e.Start(":5000"))
 }
 
-func logCheckin(c echo.Context) error {
+// Just a basic hello message :)
+func homeRoute(c echo.Context) error {
+	return c.String(http.StatusOK,"Hello there. This is the basic MSConsole server :)")
+}
+
+// TODO: Add one to the checkin count
+func logCheckinRoute(c echo.Context) error {
 	return c.String(http.StatusOK, "hi there :)")
+}
+
+// TODO: Add checkin count
+func getCheckinCount(c echo.Context) error {
+	return c.String(http.StatusOK, "Checkin Count: 0")
 }
